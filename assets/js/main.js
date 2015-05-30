b2f72a0332eb560378e72d7f2b96a2d253224b43
Colloque Tsui <colloquet@gmail.com>
2015-05-30 23:52:09 +0800

    added moment.js & small UI tweaks

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,13 @@
 // New Banana Club
-
-// Array to convert getDate() to Chinese
-var weekday = new Array(7);
-weekday[0] = "星期日";
-weekday[1] = "星期一";
-weekday[2] = "星期二";
-weekday[3] = "星期三";
-weekday[4] = "星期四";
-weekday[5] = "星期五";
-weekday[6] = "星期六";
-
-// Get yesterday's date
-var yesterday = new Date(new Date().getTime() - 24 * 60 * 60 * 1000);
-var yesterday_year = yesterday.getFullYear();
-var yesterday_month = yesterday.getMonth() + 1;
-var yesterday_date = yesterday.getDate();
+moment.locale('zh-TW');
 
 // Get today's date - as New Banana Club starts at 00:00
-var currentDate = new Date();
-var today_year = currentDate.getFullYear()
-var today_month = currentDate.getMonth() + 1
-var today_date = currentDate.getDate();
-var today = String(today_year) + ("0" + String(today_month)).slice(-2) + ("0" + String(today_date)).slice(-2);
-
-// Convert today's day to Chinese
-var yesterday_day = weekday[yesterday.getDay()];
+var today = moment().format('YYYYMMDD');
+var yesterday_day = moment().subtract(1, 'days').format('dddd');
+var yesterday_calendar = moment().subtract(1, 'days').format('L');
 
 // If yesterday is Sat or Sun, hide list and show message
-if (yesterday.getDay() == 6 || yesterday.getDay() == 0) {
+if (moment().subtract(1, 'days').format('d') == 6 || moment().subtract(1, 'days').format('d') == 0) {
     $('.today-list').css("display", "none");
     $('.na').css("display", "block");
 }
@@ -42,25 +22,16 @@ function init(date) {
 
 // When user pick a date, update list
 $(function() {
-    $.datepicker.setDefaults({
-        dateFormat: 'yy年mm月dd日'
-    });
     $("#datepicker").datepicker({
-        maxDate: yesterday_year + "年" + yesterday_month + "月" + yesterday_date + "日",
+        maxDate: new Date(),
+        minDate: '-90d',
         beforeShowDay: disableSpecificWeekDays,
         onSelect: function(date) {
-            var minDate = $(this).datepicker('getDate');
-            if (minDate) {
-                minDate.setDate(minDate.getDate() + 1);
-            }
-            var dd = String(minDate.getDate());
-            var ddd = minDate.getDate() - 1;
-            var mm = String(minDate.getMonth() + 1);
-            var yy = String(minDate.getFullYear());
-            var selectedDate = yy + ("0" + mm).slice(-2) + ("0" + dd).slice(-2);
-            var selected_day = weekday[minDate.getDay() - 1];
+            var selectedDate = moment($(this).datepicker('getDate')).add(1, 'days').format('YYYYMMDD');
+            var selectedDay = moment($(this).datepicker('getDate')).format('dddd');
+            var selectedCalendar = moment($(this).datepicker('getDate')).format('L');
             $("#list").fadeOut(function() {
-                $('#date').html("自選：" + selected_day + " " + date);
+                $('#date').html("自選：" + selectedDay + " " + selectedCalendar);
                 init(selectedDate);
                 $('.today-list').css("display", "block");
                 $('.na').css("display", "none");
@@ -83,7 +54,7 @@ function disableSpecificWeekDays(date) {
     }
 }
 
-$('#date').html("昨天：" + yesterday_day + " " + yesterday_year + "年" + ("0" + yesterday_month).slice(-2) + "月" + ("0" + yesterday_date).slice(-2) + "日");
+$('#date').html("昨天：" + yesterday_day + ' ' + yesterday_calendar);
 
 init(today);
 
